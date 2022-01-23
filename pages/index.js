69663216839292552b9e115aec58efc7d4f1fd59
Rémi Martineau <remi.martineau.dev@gmail.com>
2022-01-23 15:23:01 +0100

    custom favicon & meta title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
+import Head from "next/head";
 import Card from "../components/Card";
 import Footer from "../components/layout/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <main className='w-full h-screen flex justify-center items-center bg-grayishBlue'>
-        <Card />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <link rel='icon' type='img/png' size='32x32' href='./favicon-32x32.png' />
+        <title>QR Code Component</title>
+      </Head>
+      <div>
+        <main className='w-full h-screen flex justify-center items-center bg-grayishBlue'>
+          <Card />
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
